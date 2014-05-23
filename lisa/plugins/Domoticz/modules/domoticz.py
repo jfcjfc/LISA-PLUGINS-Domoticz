2bd296552328b70d6bdae20d1e7e0ff3f239470c
jfcjfc <jean-francois@jfchasse.fr>
2014-05-23 10:45:47 +0200

    v_140523h1045

--- lisa/plugins/Domoticz/modules/domoticz.py
+++ lisa/plugins/Domoticz/modules/domoticz.py
@@ -1,75 +1,93 @@
 # -*- coding: UTF-8 -*-
 #
-# V0.12
+# V0.3
 #
+# manque scenario et panel securité
+# manque devices -temp
+# doit marcher et a tester on/off - switch et HC 
 #
-#
-#
-# V001 juste test de son
-#  introduction de import requests (V002) et de fichier xml (V003) pour les parametres.
-# 	(importation bibliotheque 'xml.dom.minidom' ????)
-#
-#
-# from datetime import datetime
 from lisa.server.plugins.IPlugin import IPlugin
 import gettext
 import inspect
 import os
 import requests
+#
 import json
- 
-file = open('PathToJson/domoticz_param.json')
-jr = json.load(file)
+json_data=open('domoticz2.json')
+data = json.load(json_data)
+res1 = data['param_dz']
+ipc = data['configuration']
 
+# #######################
+# besoin données jsoninput
+# temperature : ['outcome']['entities']['wit/temperature']['value']
+# lieu : ['outcome']['entities']['wit/location']['value'] 
+# on/ off : ['outcome']['entities']['wit/on_off']['value'] 
+# methode :  :['outcome']['entities']["configuration"][intents]   "dz_devices": {"method":
+#
+# #######################
 
+inconf = ipc["ip"]
+pt = ipc["port"]
+
+if jsonInput['outcome']['entities']['wit/temperature']['value'] :
+	location = jsonInput['outcome']['entities']['wit/temperature']['value']
+else:
+	body = self._('pas de temperature')
+
+if jsonInput['outcome']['entities']['wit/location']['value'] :
+	location = jsonInput['outcome']['entities']['wit/location']['value']
+else:
+	body = self._('pas de lieu')
+
+if jsonInput['outcome']['entities']['wit/on_off']['value'] :
+	on_off = jsonInput['outcome']['entities']['wit/on_off']['value']
+else:
+	body = self._('pas de on/off')
+
+idex= location + "_" + on_off
+
+# #######################  EX #####
+# recherche et variable rss[]
+# "ids" : "bureau_deux_on",
+# "meth" : "switchlight", 
+# "idx" : "59",
+# "idx_sec" : "",
+# "boy" : "la lumiere bureau deux est allume"
+# #################################
+
+for rs in res1:
+	if rs['ids'] == idex :
+		rss = rs
 
 class Domoticz(IPlugin):
-    def __init__(self):
-        super(Domoticz, self).__init__()
-        self.configuration_plugin = self.mongo.lisa.plugins.find_one({"name": "Domoticz"})
-        self.path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
-            inspect.getfile(inspect.currentframe()))[0],os.path.normpath("../lang/"))))
-        self._ = translation = gettext.translation(domain='domoticz',
-                                                   localedir=self.path,
-                                                   languages=[self.configuration_lisa['lang']]).ugettext
+	def __init__(self):
+		super(Domoticz, self).__init__()
+		self.configuration_plugin = self.mongo.lisa.plugins.find_one({"name": "Domoticz"})
+		self.path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
+		inspect.getfile(inspect.currentframe()))[0],os.path.normpath("../lang/"))))
+		self._ = translation = gettext.translation(domain='domoticz',
+			localedir=self.path,
+                        languages=[self.configuration_lisa['lang']]).ugettext
 
-    def devices(self, jsonInput):
-        return {"plugin": "Domoticz",
-                "method": "devices",
-                "body": self._('temperature')
-        }
+	def devices(self, jsonInput):
+	#	http+config.ip_lan+':'+config.port+lan_path+'?type=devices&filter=temp&used=true&order=Name';
+		# resp = requests.get ('http://192.168.0.3:8080/json.htm?type=devices&filter=temp&used=true&order=Name)
+		return {"plugin": "Domoticz",
+			"method": "devices",
+			"body": self._(rss(boy))
+	}
+	
+	def switchlight(self, jsonInput):
+		resp = requests.get ('http://192.168.0.3:8080/json.htm?type=command&param=switchlight&idx=%02d&switchcmd=%s&level=0' % (rss[idx],on_off.capitalize()))
+		return {"plugin": "Domoticz",
+			"method": "switchlight",
+			"body": self._(rss(boy))
+	}
 
-    def switchlight(self, jsonInput):
-
-# def switchlight(self, jsonInput)# url6off = 'http://127.0.0.1:8080/json.hTwm?type=command&param=switchlight&idx=6&switchcmd=Off&level=0'
-# 
-#    on_off = 'off'
-#    resp = requests.get ('http://127.0.0.1:8080/json.hTwm?type=command&param=switchlight&idx=%02d&switchcmd=%s&level=0' % (dict[location],on_off.capitalize()))
-#
-	if jsonInput['outcome']['entities']['wit/location']['value'] :
-		location = jsonInput['outcome']['entities']['wit/location']['value']
-	else:
-		body = self._('probleme un')
-#
-        if jsonInput['outcome']['entities']['wit/on_off']['value'] :
-                on_off = jsonInput['outcome']['entities']['wit/location']['value']
-	else:
-		body = self._('probleme deux')
-#
-	if location = "bureau deux" and on_off ="on" :
-		resp = requests.get ('http://127.0.0.1:8080/json.htm?type=command&param=switchlight&idx=xx&switchcmd=On&level=0')
-	elif location = "bureau deux" and on_off ="off" :
-		resp = requests.get ('http://127.0.0.1:8080/json.htm?type=command&param=switchlight&idx=xx&switchcmd=Off&level=0')
-	else:
- 		body = self._('probleme trois')
-#
-        return {"plugin": "Domoticz",
-                "method": "switchlight",
-                "body": self._('lumiere')
-        }
-
-    def heatingcontrol(self, jsonInput):
-        return {"plugin": "Domoticz",
-                "method": "heatingcontrol",
-                "body": self._('chaudiere')
-        }
+	def heatingcontrol(self, jsonInput):
+		resp = requests.get ('http://192.168.0.3:8080/json.htm?type=command&param=switchlight&idx=%02d&switchcmd=%s&level=0' % (rss[idx],on_off))
+		return {"plugin": "Domoticz",
+			"method": "heatingcontrol",
+			"body": self._(rss(boy))
+	
